3ff60ad69dbab291dc2035dea0a1c0b15da567d5
toru-toru94 <hagiwarablog@gmail.com>
2025-07-19 23:18:06 +0900

    contact js hotfix

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,22 +4,24 @@ export function initContactForm(scriptURL) {
 
   form.addEventListener('submit', e => {
     e.preventDefault();
-    const formData = new FormData(form);
 
-    fetch(scriptURL, { method: 'POST', body: formData })
-      .then(response => response.text())  // レスポンスをテキストで受け取る
+    fetch(scriptURL, {
+      method: 'POST',
+      mode: 'cors',
+      body: new FormData(form)  // フォームデータ送信
+    })
+      .then(res => res.json())  // JSONとして受け取る
       .then(result => {
-        if (result.trim() === 'success') { // "success" ならリダイレクト
+        if (result.status === 'success') {
           window.location.href = '/thanks.html';
         } else {
-          // 予期しないレスポンスが返った場合（古いデプロイなど）
           console.warn('Unexpected response:', result);
-          alert('送信処理で問題が発生しました。');
+          alert('送信に失敗しました。');
         }
       })
       .catch(error => {
-        alert('通信エラーが発生しました。');
         console.error(error);
+        alert('通信エラーが発生しました。');
       });
   });
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { initWorksSlider } from './slider.js';
 import { initContactForm } from './contact.js';
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwsSI8I6a-2pOw85QiYHkedQDKPNiE3_k62TFWtfoWBCpeELW1KMgypiQIDEetcFvvdNw/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyQC6ftuISZlUmARB-xM9wW2-Sr9BG_d2fLNBuXRvhVavNY2nhWrxMBnQRXOSfcxStWQQ/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
   initWorksSlider();
